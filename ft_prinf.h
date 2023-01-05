194dbfc2b1de97b1a2e5e56ecddc0c70e78a06ea
GugzNutz <77459593+GugzNutz@users.noreply.github.com>
2023-01-05 13:15:53 +0000

    Add files via upload

--- ft_prinf.h
+++ ft_prinf.h
@@ -3,19 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_prinf.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gfilipe- <gfilipe-@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: gfilipe- <gfilipe-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 16:09:01 by gfilipe-          #+#    #+#             */
-/*   Updated: 2022/12/07 16:11:15 by gfilipe-         ###   ########.fr       */
+/*   Updated: 2023/01/05 13:15:29 by gfilipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
-
-# include <stdarg.h>
-# include <unistd.h>
+/*stdarg is the library to use for a variable number of arguments*/
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int	ft_prinf(const char *format, ...);
 
+int	max(int num_args, ...);
+
+va_list args;
+
+va_start(args, *format);
+
+for (int i = 0; i < num_args; i++)
+{
+	int	x = va_arg(args, int);
+	printf("x: %d\n", x);
+}
+
+va_end(args)/*ends my acess to the list of arguments*/
 #endif
